e90b115f6eb1a979d0b1ebed9b373c054748ff82
Cliff Saporta Cheng <ccfcheng@gmail.com>
2015-08-03 15:39:52 -0700

    Added toggle for advanced search
    Checkbox toggles appearance of advanced search options
    Basic options now populate scope variables with search strings conforming to Yelp API

--- client/app.js
+++ client/app.js
@@ -3,13 +3,44 @@ var yep = angular.module('yep', []);
 yep.controller('MainController', function($scope, Search) {
   angular.extend($scope, Search);
   // this string represents valid Yelp restaurant categories
+  $scope.categoryHash = {
+
+  };
+  
   $scope.category_filter = '';
+  $scope.searchLocation = '';
+  $scope.radius_filter;
+  
+  // $scope.advancedSearch = true;
+
+  // $scope.toggleAdvancedMode = function() {
+
+  //   $scope.advancedSearch = !$scope.advancedSearch;
+  // };
+
 })
 
 .factory('Search', function(){
   return {
+    // toggleAdvancedMode: function() {
+    //   if (this.advancedSearch) {
+    //     this.advancedSearch = false;
+    //   } else {
+    //     this.advancedSearch = true;
+    //   }
+    // },
+
     setCategory: function(term) {
-      this.category_filter = term;
+      this.category_filter = 'category_filter=' + term;
+    },
+
+    setLocation: function() {
+      this.searchLocation = 'location=' + this.currentLocation;
+    },
+
+    setRadius: function(miles) {
+      this.radius_filter = 'radius_filter=' + (miles * 800); 
     }
+
   };
 });

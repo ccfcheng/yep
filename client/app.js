dce8e8bc580fc7adc0e52ecca85b8cef95666f3d
Cliff Saporta Cheng <ccfcheng@gmail.com>
2015-08-03 14:05:42 -0700

    Minor changes to .gitignore and app.js

--- client/app.js
+++ client/app.js
@@ -1,1 +1,10 @@
-var test = 5;
+var yep = angular.module('yep', []);
+
+yep.controller('MainController', function($scope, Search) {
+  angular.extend($scope, Search);
+  // $scope.searchTerm = 'hello';
+})
+
+.factory('Search', function(){
+  return {};
+});
